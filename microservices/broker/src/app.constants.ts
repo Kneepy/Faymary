dada6fc9300355e3d618f23fb2d94fea65ea0ba3
Kneepy <kneepydev@gmail.com>
2023-02-22 18:31:40 +0800

    Сделаны конфигурации всех микросервисов в брокере запросов

--- microservices/broker/src/app.constants.ts
+++ microservices/broker/src/app.constants.ts
@@ -1,13 +1,40 @@
-import { Transport } from "@nestjs/microservices";
 import * as path from "path"
 import * as process from "process";
 
 export const APP_PORT = 5000;
 
-// LIKES PACKAGE
-export const LIKES_MODULE_PORT = 5001;
-export const LIKES_MODULE = "LIKES_MODULE"
-export const LIKES_MODULE_PKG = "likes";
-export const LIKES_MODULE_HOST = `localhost:${LIKES_MODULE_PORT}`;
-export const LIKES_MODULE_PROTO = path.join(process.cwd(), "src/proto/likes.proto");
-export const LIKES_SERVICE = "LikesService";
+export const DEFAULT_HOST = 'localhost'
+interface ConfingModuleArgs {
+    port: number,
+    pkgName: string,
+    host: string,
+    protoPath: string,
+    serviceName: string
+}
+interface ConfigModule {
+    PORT: number,
+    PACKAGE: string,
+    HOST: string,
+    PROTO: string,
+    SERVICE: string
+}
+
+export const getModuleConfig = (data: ConfingModuleArgs): ConfigModule => ({
+    PORT: data.port,
+    PACKAGE: data.pkgName,
+    HOST: `${data.host}:${data.port}`,
+    PROTO: data.protoPath,
+    SERVICE: data.serviceName
+})
+const getProtoPath = (protoFileName: string) => path.join(process.cwd(), `src/proto/${protoFileName}.proto`)
+
+export const USER_MODULE_CONFIG = getModuleConfig({port: 5002, pkgName: "user", host: DEFAULT_HOST, protoPath: getProtoPath("user"), serviceName: "UserService"})
+export const STORIES_MODULE_CONFIG = getModuleConfig({port: 5009, pkgName: "stories", host: DEFAULT_HOST, protoPath: getProtoPath("stories"), serviceName: "StoriesService"})
+export const STORE_MODULE_CONFIG = getModuleConfig({port: 5008, pkgName: "store", host: DEFAULT_HOST, protoPath: getProtoPath("store"), serviceName: "StoreService"})
+export const SESSION_MODULE_CONFIG = getModuleConfig({port: 5007, pkgName: "session", host: DEFAULT_HOST, protoPath: getProtoPath("session"), serviceName: "SessionService"})
+export const POST_MODULE_CONFIG = getModuleConfig({port: 5006, pkgName: "post", host: DEFAULT_HOST, protoPath: getProtoPath("post"), serviceName: "PostService"})
+export const NOTIFICATIONS_MODULE_CONFIG = getModuleConfig({port: 5005, pkgName: "notifications", host: DEFAULT_HOST, protoPath: getProtoPath("notification"), serviceName: "NotificationsService"})
+export const MESSAGES_MODULE_CONFIG = getModuleConfig({port: 5004, pkgName: "messages", host: DEFAULT_HOST, protoPath: getProtoPath("messages"), serviceName: "MessagesSerivce"})
+export const DIALOGS_MODULE_CONFIG = getModuleConfig({port: 5003, pkgName: "dialogs", host: DEFAULT_HOST, protoPath: getProtoPath("dialogs"), serviceName: "DialogsService"})
+export const LIKES_MODULE_CONFIG = getModuleConfig({port: 5001, pkgName: "likes", host: DEFAULT_HOST, protoPath: getProtoPath("likes"), serviceName: "LikesService"})
+export const COMMENTS_MODULE_CONFIG = getModuleConfig({port: 5011, pkgName: "comments", host: DEFAULT_HOST, protoPath: getProtoPath("comments"), serviceName: "CommentsService"})
--- microservices/broker/src/clients/likes.client.ts
+++ microservices/broker/src/clients/likes.client.ts
@@ -1,20 +1,31 @@
-import {Inject, Injectable, OnModuleInit} from "@nestjs/common";
-import { ClientGrpc } from "@nestjs/microservices";
-import {LIKES_MODULE, LIKES_SERVICE} from "../app.constants";
+import {Injectable, OnModuleInit} from "@nestjs/common";
+import {Client, ClientGrpc, Transport} from "@nestjs/microservices";
 import {AddLikeDTO, Like, LikesServiceClient} from "src/proto/likes";
-import {Observable} from "rxjs";
+import {LIKES_MODULE_CONFIG} from "../app.constants";
 
 @Injectable()
 export class LikesClient implements OnModuleInit {
-    constructor(@Inject(LIKES_MODULE) private client: ClientGrpc) {}
+
+    @Client({
+        transport: Transport.GRPC,
+        options: {
+            url: LIKES_MODULE_CONFIG.HOST,
+            package: LIKES_MODULE_CONFIG.PACKAGE,
+            protoPath: LIKES_MODULE_CONFIG.PROTO,
+            loader: {
+                keepCase: true
+            }
+        }
+    })
+    client: ClientGrpc
 
     private likesService: LikesServiceClient;
 
     onModuleInit(): any {
-        this.likesService = this.client.getService<LikesServiceClient>(LIKES_SERVICE)
+        this.likesService = this.client.getService<LikesServiceClient>(LIKES_MODULE_CONFIG.SERVICE)
     }
 
-    addLike(data: AddLikeDTO): Observable<Like> {
-        return this.likesService.addLike(data)
+    addLike(data: AddLikeDTO): Promise<Like> {
+        return this.likesService.addLike(data).toPromise()
     }
 }
