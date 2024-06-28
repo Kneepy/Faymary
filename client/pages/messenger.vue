c8b030e7fb22a924eb8771639f983190c7e5e1a1
Parside01 <vadimakovlev79449@gmail.com>
2024-06-28 13:23:25 +0800

    уцацуау

--- client/pages/messenger.vue
+++ client/pages/messenger.vue
@@ -3,7 +3,7 @@
 import { ROUTES } from "~/assets/constants/routes.constants";
 
 definePageMeta({
-    requiredAuth: true, // это только на время разработки, так должно быть true
+    requiredAuth: false, // это только на время разработки, так должно быть true
     name: ROUTES.MESSENGER,
     layout: false // только на время разработки, но в обычное время default или просто удалить layout
 })
@@ -14,8 +14,29 @@ useHead({
 </script>
 
 <template>
+    <div class="messenger">
+        <div class="dialogs">
+
+        </div>
+        <div class="messages"></div>
+    </div>
 </template>
 
 <style scoped lang="scss">
-
+.messenger {
+    width: 900px;
+    margin: 0 auto;
+    display: flex;
+    height: 100px;
+    border: 1px solid $border_1;
+    border-radius: 5px;
+    padding: 5px 0;
+    .dialogs {
+        flex: 0.5;
+        border-right: 1px solid $primary_border;
+    }
+    .messages {
+        flex: 1;
+    }
+}
 </style>
