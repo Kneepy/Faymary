522c49781636ea8ccd4b80f19a1b75cee909956f
Kneepy <ilyafamin4@gmail.com>
2024-06-28 23:13:11 +0800

    Начата работа над страницей мессенджера

--- client/components/shared/Avatar.vue
+++ client/components/shared/Avatar.vue
@@ -1,34 +1,43 @@
 <script setup lang="ts">
 const props = defineProps<{
-    size: number,
+    size: number
     /**
      * Может быть как ссылкой так и id файла из files api
      */
     href?: string
-    /*
+    /**
     * Если значение не передано, то аватарка будет круглой
     * Если значение передано то она будет квадратной с закруглениями в ${ rounded } px
     */
     rounded?: number
+
+    /**
+    * Образ юзера необходимый если у него нет аватарки
+    * Чтобы взять первую букву имени
+    */
+    userName: string
 }>()
 
-const userStore = useUserStore()
 const config = useRuntimeConfig()
 const img = computed(() => props.href ? (!!props.href?.split("http")[1] ? props.href : config.public.filesApiURL + props.href) : null)
-const fullName = computed(() => userStore.me.fullName ?? "")
 </script>
 
 <template>
     <div
         v-if="!!img"
-        class="avatar"
-        :style="{width: props.size + `px`, height: size + `px`, backgroundImage: `url(${img})`}"
+        class="avatar noselect"
+        :style="{
+            width: props.size + `px`,
+            height: props.size + `px`,
+            backgroundImage: `url(${img})`,
+            borderRadius: !!props.rounded ? props.rounded + `px` : `50%`
+        }"
     ></div>
     <div
         v-else
-        class="avatar empty"
-        :style="{ borderRadius: props.rounded ? props.rounded + `px` : `50%` }"
-    >{{ fullName[0] }}</div>
+        class="avatar empty noselect"
+        :style="{ borderRadius: !!props.rounded ? props.rounded + `px` : `50%` }"
+    >{{ props.userName[0] }}</div>
 </template>
 
 <style scoped lang="scss">
@@ -46,6 +55,7 @@ const fullName = computed(() => userStore.me.fullName ?? "")
         justify-content: center;
         color: $black;
         font-size: 20px;
+        user-select: none;
     }
 }
 </style>
--- client/pages/messenger.vue
+++ client/pages/messenger.vue
@@ -11,12 +11,54 @@ useHead({
     title: "Сообщения"
 })
 
+const cropMessage = (msg: string, maxLength: number) => {
+    if (msg.length < maxLength) return msg
+
+    return msg.slice(0, maxLength)+ "..."
+}
+const testMsg = "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci consequatur eligendi, id nobis repellat rerum sint tempore vitae. Beatae blanditiis ipsum molestiae! Corporis dolorum eos eveniet nemo porro quasi, vel."
 </script>
 
 <template>
     <div class="messenger">
-        <div class="dialogs">
-
+        <div class="left-bar">
+            <div class="top-menu">
+                <div class="options">
+                    <button>
+                        <span class="material-symbols-rounded">star</span>
+                    </button>
+                    <button>
+                        <span class="material-symbols-rounded">settings</span>
+                    </button>
+                    <button>
+                        <span class="material-symbols-rounded">block</span>
+                    </button>
+                </div>
+                <div class="search">
+                    <input type="text" placeholder="Найдите нужный вам диалог!">
+                    <button class="search-btn">
+                        <span class="material-symbols-rounded">hexagon</span>
+                    </button>
+                </div>
+            </div>
+            <div class="dialogs">
+                <div class="dialog" v-for="i in Array(2)">
+                    <Avatar :size=45 :user-name="`Ilya`" :href="`https://images.unsplash.com/photo-1719430074740-a5ee49a67d45?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D`" />
+                    <div class="dialog__info">
+                        <div class="name">Ilya Famin</div>
+                        <div class="last-message overflow" :style="{}">{{ cropMessage(testMsg, 40) }}</div>
+                        <div class="last-message-time">16:27</div>
+                    </div>
+                </div>
+                <div class="dialog" v-for="i in Array(2)">
+                    <Avatar :size=45 :user-name="`Ilya`" :href="`https://images.unsplash.com/photo-1719430074740-a5ee49a67d45?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D`" />
+                    <div class="dialog__info">
+                        <div class="name">Alex Korf</div>
+                        <div class="last-message">Hello World</div>
+                        <div class="last-message-time">16:24</div>
+                    </div>
+                </div>
+            </div>
         </div>
         <div class="messages"></div>
     </div>
@@ -24,16 +66,148 @@ useHead({
 
 <style scoped lang="scss">
 .messenger {
-    width: 900px;
+    width: 1200px;
     margin: 0 auto;
     display: flex;
-    height: 100px;
     border: 1px solid $border_1;
     border-radius: 5px;
     padding: 5px 0;
-    .dialogs {
+    margin-top: 100px; // это нужно убрать после того как закончу разработку чата
+
+    .left-bar {
         flex: 0.5;
         border-right: 1px solid $primary_border;
+        padding: 10px;
+        display: flex;
+        flex-direction: column;
+
+        .top-menu {
+            .search {
+                display: flex;
+                position: relative;
+                input {
+                    margin-bottom: 10px;
+                    border-radius: 10px;
+                    outline: none;
+                    flex: 1;
+                    padding: 13px 20px 13px 46px;
+                    background-color: $transparent_hover_background;
+                    color: $white;
+                    border: 1px solid $primary_border;
+                    &::placeholder {
+                        font-size: 16px;
+                        display: flex;
+                        align-items: center;
+                        color: $gray_1;
+                    }
+                }
+                &-btn {
+                    position: absolute;
+                    cursor: pointer;
+                    display: flex;
+                    align-items: center;
+                    justify-content: center;
+                    background-color: transparent;
+                    border: none;
+                    outline: none;
+                    top: 11px;
+                    transition: 1.2s;
+                    left: 13px;
+                    &:hover {
+                        transform: rotate(90deg);
+                        transition: 1.2s;
+                        span {
+                            color: $gray;
+                            transition: 200ms;
+                        }
+                    }
+                    span {
+                        color: $gray_1;
+                        transition: 200ms;
+                    }
+                }
+            }
+
+            .options {
+                display: flex;
+                justify-content: end;
+                margin-bottom: 10px;
+                button {
+                    display: flex;
+                    align-items: center;
+                    justify-content: center;
+                    background-color: transparent;
+                    border: none;
+                    cursor: pointer;
+                    padding: 5px;
+                    border-radius: 50%;
+                    &:hover {
+                        background-color: $transparent_button_hover_1;
+                    }
+                    span {
+                        color: $gray_1;
+                        font-size: 20px;
+                        font-variation-settings:
+                            'FILL' 1,
+                            'wght' 400,
+                            'GRAD' 0,
+                            'opsz' 24
+                        ;
+                    }
+                }
+            }
+        }
+        .dialogs {
+            .dialog {
+                display: flex;
+                align-items: center;
+                background-color: $transparent_hover_background;
+                padding: 15px;
+                border-radius: 10px;
+                cursor: pointer;
+                transition: 200ms;
+                margin-bottom: 8px;
+                transform: scale(0.99);
+                font-size: 18px;
+                position: relative;
+
+                &:hover {
+                    background-color: $transparent_button_hover_17;
+                    transition: 200ms;
+                    transform: scale(1);
+                }
+                &__info {
+                    margin-left: 20px;
+
+                    .name {
+                        color: $white;
+                        text-overflow: ellipsis;
+                        max-width: 260px;
+                        text-wrap: nowrap;
+                        overflow: hidden;
+                    }
+                    .last-message {
+                        color: $gray;
+                        text-overflow: ellipsis;
+                        max-width: 260px;
+                        text-wrap: nowrap;
+                        overflow: hidden;
+                        font-size: 16px;
+                        &.overflow {
+                            overflow: initial;
+                            text-wrap: initial;
+                        }
+                    }
+                    .last-message-time {
+                        position: absolute;
+                        top: 15px;
+                        right: 15px;
+                        font-size: 15px;
+                        color: $gray_1;
+                    }
+                }
+            }
+        }
     }
     .messages {
         flex: 1;
